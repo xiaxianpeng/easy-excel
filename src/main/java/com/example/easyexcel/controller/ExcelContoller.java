5e30626e0ada558e982b51e1c529755beaf6bb97
xiapeng <xiaxianpeng@autohome.com.cn>
2023-09-26 18:00:51 +0800

    测试

--- src/main/java/com/example/easyexcel/controller/ExcelContoller.java
+++ src/main/java/com/example/easyexcel/controller/ExcelContoller.java
@@ -1,11 +1,19 @@
 package com.example.easyexcel.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.example.easyexcel.listener.UserAnalysisEventListener;
 import com.example.easyexcel.model.UserModel;
 import com.example.easyexcel.util.ExcelHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +24,10 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/excel")
 public class ExcelContoller {
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @PostMapping("/syncReadExcel")
     public Object syncReadExcel(@RequestParam("uploadingFile") MultipartFile uploadingFile) throws IOException {
         return ExcelHelper.syncReadExcel(uploadingFile.getInputStream(), UserModel.class, ExcelTypeEnum.XLSX);
@@ -30,5 +42,70 @@ public class ExcelContoller {
     @GetMapping("/log")
     public String log() {
         return "success";
+    }
+
+    @GetMapping("/export")
+    public String export() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("user10.xlsx");
+            List<UserModel> data = UserModel.generateUserList(10);
+            ExcelHelper.exportExcel(fileOutputStream, ExcelTypeEnum.XLSX, UserModel.class, data);
+            return "success";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "failure";
+        }
+    }
+
+    @GetMapping("/exportExcelWithTemplate")
+    public String exportExcelWithTemplate() {
+        try {
+            Resource resource = resourceLoader.getResource("classpath:templates/user-export-template.xlsx");
+            OutputStream outputStream = new FileOutputStream("user100.xlsx");
+            InputStream templateInputStream = resource.getInputStream();
+            List<UserModel> data = UserModel.generateUserList(100);
+            ExcelHelper.exportExcelWithTemplate(outputStream, ExcelTypeEnum.XLSX, UserModel.class, templateInputStream, data);
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "failure";
+        }
+    }
+
+    @GetMapping("/exportMore")
+    public String exportMore() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("user100000.xlsx");
+            List<UserModel> data = UserModel.generateUserList(100000);
+            ExcelHelper.exportExcel(fileOutputStream,
+                    ExcelTypeEnum.XLSX,
+                    UserModel.class,
+                    writeSheetHolder -> writeSheetHolder.writeSheetData(data));
+            return "success";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "failure";
+        }
+    }
+
+    @GetMapping("/exportExcelMoreWithTemplate")
+    public String exportExcelMoreWithTemplate() {
+        try {
+            Resource resource = resourceLoader.getResource("classpath:templates/user-export-template.xlsx");
+            OutputStream outputStream = new FileOutputStream("user150000.xlsx");
+            InputStream templateInputStream = resource.getInputStream();
+            List<UserModel> data = UserModel.generateUserList(150000);
+            ExcelHelper.exportExcelWithTemplate(outputStream,
+                    ExcelTypeEnum.XLSX,
+                    UserModel.class,
+                    templateInputStream,
+                    fillSheetHolder -> {
+                        fillSheetHolder.writeSheetData(data);
+                    });
+            return "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "failure";
+        }
     }
 }
--- src/main/java/com/example/easyexcel/model/UserModel.java
+++ src/main/java/com/example/easyexcel/model/UserModel.java
@@ -1,6 +1,10 @@
 package com.example.easyexcel.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Random;
 
 import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Getter;
@@ -17,4 +21,48 @@ public class UserModel {
     private Integer sex;
     @ExcelProperty("BIRTHDAY")
     private Date birthday;
+
+    public static List<UserModel> generateUserList(Integer count) {
+        List<UserModel> users = new ArrayList<UserModel>();
+        for (int i = 0; i < count; i++) {
+            users.add(generateUser());
+        }
+        return users;
+    }
+
+    public static UserModel generateUser() {
+        UserModel user = new UserModel();
+        Random rand = new Random();
+        // Generate random IDs in the range 0-100000
+        user.setId(rand.nextInt(100001));
+        // Generate a random string of length 10 for the name
+        user.setName(generateRandomString(10));
+        // Generate random age in the range 0-200
+        user.setSex(rand.nextInt(201));
+        // Set today's date
+        user.setBirthday(generateRandomDateAfter2000());
+        return user;
+    }
+
+    private static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder result = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i < length; i++) {
+            result.append(characters.charAt(rand.nextInt(characters.length())));
+        }
+        return result.toString();
+    }
+
+    private static Date generateRandomDateAfter2000() {
+        GregorianCalendar gc = new GregorianCalendar();
+        Random rand = new Random();
+        //for years 2000 to 2021
+        int year = 2000 + rand.nextInt(22);
+        gc.set(GregorianCalendar.YEAR, year);
+        //1-365
+        int dayOfYear = rand.nextInt(gc.getActualMaximum(GregorianCalendar.DAY_OF_YEAR)) + 1;
+        gc.set(GregorianCalendar.DAY_OF_YEAR, dayOfYear);
+        return gc.getTime();
+    }
 }
